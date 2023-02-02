1bedbd5c06c39bb23357267061bbc64aabc3460e
ahmetcetnn <ahmetlaw@outlook.com>
2023-02-02 22:37:48 +0300

    slider completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import React from 'react'
-import { SliderData } from './Components/SliderData'
-
-
+import React, { useState } from 'react'
+import Data from './Components/Data'
 function App() {
+
+  const [theme,setTheme] =useState(true)
   return (
-    <div>
-    {SliderData.map((index) => {
-      return(
-        <div>
-      <h1>{SliderData.Title}</h1>
-      <a>{SliderData.Actors}</a>
-      <a>{SliderData.Plot}</a>
-      <img src={SliderData.Poster} alt="poster"/>
-      </div>
-      )
-    })}
-   
+    <div className={theme ? "light"  : "dark" }>
+     <div className='btn'> <button onClick={() => setTheme(!theme)}>*</button> </div>
+      <Data/>
     </div>
   )
 }
 
 export default App
+
---
+++ src/Components/Data.jsx
@@ -0,0 +1,59 @@
+import "../App.css"
+import SliderData  from './SliderData'
+import '@splidejs/react-splide/css'
+import styled from "styled-components"
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+
+
+function Data() {
+
+
+  return <div>
+
+
+<Wrapper>
+
+
+    <Splide options={{
+      perPage:4,
+      arrows:false,
+      pagination:false,
+      drag:'free',
+      gap:'5rem',
+    }}>
+    {SliderData.map((slide,index) => {
+      return(
+        <SplideSlide>
+            <Card>
+      <h1>{slide.title}</h1>
+      <h2>{slide.genres}</h2>
+      <h3>{slide.actors}</h3>
+      <h5>{slide.plot}</h5>
+      
+      </Card>
+      <div className="resim">
+      <img src={slide.posterUrl} alt="poster"/>
+      </div>
+      </SplideSlide>
+      )
+    })}
+   
+    </Splide>
+    </Wrapper>
+    </div>
+}
+const Wrapper =styled.div`
+margin:4rem 1rem;`;
+
+const Card =styled.div`
+min-height:25rem;
+border-radius:2rem;
+overflow:hidden;
+position:relative;
+  `
+
+
+
+
+
+export default Data
